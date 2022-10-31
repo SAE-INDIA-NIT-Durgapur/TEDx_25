43578018475e65b7645f5614052ac038eda47d1b
dhrubajit160 <109053603+dhrubajit160@users.noreply.github.com>
2022-10-31 20:16:31 +0530

    members-page update

--- src/Members/Members.jsx
+++ src/Members/Members.jsx
@@ -1,7 +1,317 @@
 import React from 'react'
+import './Members.css';
 
 export default function Members() {
   return (
-    <div>Members</div>
+    <div>    <section className="team-page">
+    <div className="heading">
+        <h1>Our Team</h1>
+    </div>
+    <div className="main-container">
+        <div className="row">
+
+            <div className="col-md-3">
+                <div className="team-member">
+                    <figure>
+                        <img src="https://i.pinimg.com/originals/f5/a7/f2/f5a7f286dca7652e15cf413e5efe2702.png" />
+                        <ul>
+                          <li><a href="https://www.facebook.com/" target="_blank"><i className="fa fa-facebook fa-lg"></i></a></li>
+                          <li><a href="https://www.linkedin.com/" target="_blank"><i className="fa fa-linkedin fa-lg"></i></a></li>
+                          <li><a href="mailto:"><i className="fa fa-envelope fa-lg"></i></a></li>
+                        </ul>
+                    </figure>
+                    <h4>Tony Stark</h4>
+                    <p>Web Developer</p>
+                </div>
+            </div>
+
+            <div className="col-md-3">
+                <div className="team-member">
+                    <figure>
+                        <img src="https://i.pinimg.com/originals/f5/a7/f2/f5a7f286dca7652e15cf413e5efe2702.png" />
+                        <ul>
+                          <li><a href="https://www.facebook.com/" target="_blank"><i className="fa fa-facebook fa-lg"></i></a></li>
+                          <li><a href="https://www.linkedin.com/" target="_blank"><i className="fa fa-linkedin fa-lg"></i></a></li>
+                          <li><a href="mailto:"><i className="fa fa-envelope fa-lg"></i></a></li>
+                        </ul>
+                    </figure>
+                    <h4>Tony Stark</h4>
+                    <p>Web Developer</p>
+                </div>
+            </div>
+
+            <div className="col-md-3">
+                <div className="team-member">
+                    <figure>
+                        <img src="https://i.pinimg.com/originals/f5/a7/f2/f5a7f286dca7652e15cf413e5efe2702.png" />
+                        <ul>
+                          <li><a href="https://www.facebook.com/" target="_blank"><i className="fa fa-facebook fa-lg"></i></a></li>
+                          <li><a href="https://www.linkedin.com/" target="_blank"><i className="fa fa-linkedin fa-lg"></i></a></li>
+                          <li><a href="mailto:"><i className="fa fa-envelope fa-lg"></i></a></li>
+                        </ul>
+                    </figure>
+                    <h4>Tony Stark</h4>
+                    <p>Web Developer</p>
+                </div>
+            </div>
+
+            <div className="col-md-3">
+                <div className="team-member">
+                    <figure>
+                        <img src="https://i.pinimg.com/originals/f5/a7/f2/f5a7f286dca7652e15cf413e5efe2702.png" />
+                        <ul>
+                          <li><a href="https://www.facebook.com/" target="_blank"><i className="fa fa-facebook fa-lg"></i></a></li>
+                          <li><a href="https://www.linkedin.com/" target="_blank"><i className="fa fa-linkedin fa-lg"></i></a></li>
+                          <li><a href="mailto:"><i className="fa fa-envelope fa-lg"></i></a></li>
+                        </ul>
+                    </figure>
+                    <h4>Tony Stark</h4>
+                    <p>Web Developer</p>
+                </div>
+            </div>
+
+            <div className="col-md-3">
+                <div className="team-member">
+                    <figure>
+                        <img src="https://i.pinimg.com/originals/f5/a7/f2/f5a7f286dca7652e15cf413e5efe2702.png" />
+                        <ul>
+                          <li><a href="https://www.facebook.com/" target="_blank"><i className="fa fa-facebook fa-lg"></i></a></li>
+                          <li><a href="https://www.linkedin.com/" target="_blank"><i className="fa fa-linkedin fa-lg"></i></a></li>
+                          <li><a href="mailto:"><i className="fa fa-envelope fa-lg"></i></a></li>
+                        </ul>
+                    </figure>
+                    <h4>Tony Stark</h4>
+                    <p>Web Developer</p>
+                </div>
+            </div>
+
+            <div className="col-md-3">
+                <div className="team-member">
+                    <figure>
+                        <img src="https://i.pinimg.com/originals/f5/a7/f2/f5a7f286dca7652e15cf413e5efe2702.png" />
+                        <ul>
+                          <li><a href="https://www.facebook.com/" target="_blank"><i className="fa fa-facebook fa-lg"></i></a></li>
+                          <li><a href="https://www.linkedin.com/" target="_blank"><i className="fa fa-linkedin fa-lg"></i></a></li>
+                          <li><a href="mailto:"><i className="fa fa-envelope fa-lg"></i></a></li>
+                        </ul>
+                    </figure>
+                    <h4>Tony Stark</h4>
+                    <p>Web Developer</p>
+                </div>
+            </div>
+
+            <div className="col-md-3">
+                <div className="team-member">
+                    <figure>
+                        <img src="https://i.pinimg.com/originals/f5/a7/f2/f5a7f286dca7652e15cf413e5efe2702.png" />
+                        <ul>
+                          <li><a href="https://www.facebook.com/" target="_blank"><i className="fa fa-facebook fa-lg"></i></a></li>
+                          <li><a href="https://www.linkedin.com/" target="_blank"><i className="fa fa-linkedin fa-lg"></i></a></li>
+                          <li><a href="mailto:"><i className="fa fa-envelope fa-lg"></i></a></li>
+                        </ul>
+                    </figure>
+                    <h4>Tony Stark</h4>
+                    <p>Web Developer</p>
+                </div>
+            </div>
+
+            <div className="col-md-3">
+                <div className="team-member">
+                    <figure>
+                        <img src="https://i.pinimg.com/originals/f5/a7/f2/f5a7f286dca7652e15cf413e5efe2702.png" />
+                        <ul>
+                          <li><a href="https://www.facebook.com/" target="_blank"><i className="fa fa-facebook fa-lg"></i></a></li>
+                          <li><a href="https://www.linkedin.com/" target="_blank"><i className="fa fa-linkedin fa-lg"></i></a></li>
+                          <li><a href="mailto:"><i className="fa fa-envelope fa-lg"></i></a></li>
+                        </ul>
+                    </figure>
+                    <h4>Tony Stark</h4>
+                    <p>Web Developer</p>
+                </div>
+            </div>
+
+            <div className="col-md-3">
+                <div className="team-member">
+                    <figure>
+                        <img src="https://i.pinimg.com/originals/f5/a7/f2/f5a7f286dca7652e15cf413e5efe2702.png" />
+                        <ul>
+                          <li><a href="https://www.facebook.com/" target="_blank"><i className="fa fa-facebook fa-lg"></i></a></li>
+                          <li><a href="https://www.linkedin.com/" target="_blank"><i className="fa fa-linkedin fa-lg"></i></a></li>
+                          <li><a href="mailto:"><i className="fa fa-envelope fa-lg"></i></a></li>
+                        </ul>
+                    </figure>
+                    <h4>Tony Stark</h4>
+                    <p>Web Developer</p>
+                </div>
+            </div>
+
+            <div className="col-md-3">
+                <div className="team-member">
+                    <figure>
+                        <img src="https://i.pinimg.com/originals/f5/a7/f2/f5a7f286dca7652e15cf413e5efe2702.png" />
+                        <ul>
+                          <li><a href="https://www.facebook.com/" target="_blank"><i className="fa fa-facebook fa-lg"></i></a></li>
+                          <li><a href="https://www.linkedin.com/" target="_blank"><i className="fa fa-linkedin fa-lg"></i></a></li>
+                          <li><a href="mailto:"><i className="fa fa-envelope fa-lg"></i></a></li>
+                        </ul>
+                    </figure>
+                    <h4>Tony Stark</h4>
+                    <p>Web Developer</p>
+                </div>
+            </div>
+
+            <div className="col-md-3">
+                <div className="team-member">
+                    <figure>
+                        <img src="https://i.pinimg.com/originals/f5/a7/f2/f5a7f286dca7652e15cf413e5efe2702.png" />
+                        <ul>
+                          <li><a href="https://www.facebook.com/" target="_blank"><i className="fa fa-facebook fa-lg"></i></a></li>
+                          <li><a href="https://www.linkedin.com/" target="_blank"><i className="fa fa-linkedin fa-lg"></i></a></li>
+                          <li><a href="mailto:"><i className="fa fa-envelope fa-lg"></i></a></li>
+                        </ul>
+                    </figure>
+                    <h4>Tony Stark</h4>
+                    <p>Web Developer</p>
+                </div>
+            </div>
+
+            <div className="col-md-3">
+                <div className="team-member">
+                    <figure>
+                        <img src="https://i.pinimg.com/originals/f5/a7/f2/f5a7f286dca7652e15cf413e5efe2702.png" />
+                        <ul>
+                          <li><a href="https://www.facebook.com/" target="_blank"><i className="fa fa-facebook fa-lg"></i></a></li>
+                          <li><a href="https://www.linkedin.com/" target="_blank"><i className="fa fa-linkedin fa-lg"></i></a></li>
+                          <li><a href="mailto:"><i className="fa fa-envelope fa-lg"></i></a></li>
+                        </ul>
+                    </figure>
+                    <h4>Tony Stark</h4>
+                    <p>Web Developer</p>
+                </div>
+            </div>
+
+            <div className="col-md-3">
+                <div className="team-member">
+                    <figure>
+                        <img src="https://i.pinimg.com/originals/f5/a7/f2/f5a7f286dca7652e15cf413e5efe2702.png" />
+                        <ul>
+                          <li><a href="https://www.facebook.com/" target="_blank"><i className="fa fa-facebook fa-lg"></i></a></li>
+                          <li><a href="https://www.linkedin.com/" target="_blank"><i className="fa fa-linkedin fa-lg"></i></a></li>
+                          <li><a href="mailto:"><i className="fa fa-envelope fa-lg"></i></a></li>
+                        </ul>
+                    </figure>
+                    <h4>Tony Stark</h4>
+                    <p>Web Developer</p>
+                </div>
+            </div>
+
+            <div className="col-md-3">
+                <div className="team-member">
+                    <figure>
+                        <img src="https://i.pinimg.com/originals/f5/a7/f2/f5a7f286dca7652e15cf413e5efe2702.png" />
+                        <ul>
+                          <li><a href="https://www.facebook.com/" target="_blank"><i className="fa fa-facebook fa-lg"></i></a></li>
+                          <li><a href="https://www.linkedin.com/" target="_blank"><i className="fa fa-linkedin fa-lg"></i></a></li>
+                          <li><a href="mailto:"><i className="fa fa-envelope fa-lg"></i></a></li>
+                        </ul>
+                    </figure>
+                    <h4>Tony Stark</h4>
+                    <p>Web Developer</p>
+                </div>
+            </div>
+
+            <div className="col-md-3">
+                <div className="team-member">
+                    <figure>
+                        <img src="https://i.pinimg.com/originals/f5/a7/f2/f5a7f286dca7652e15cf413e5efe2702.png" />
+                        <ul>
+                          <li><a href="https://www.facebook.com/" target="_blank"><i className="fa fa-facebook fa-lg"></i></a></li>
+                          <li><a href="https://www.linkedin.com/" target="_blank"><i className="fa fa-linkedin fa-lg"></i></a></li>
+                          <li><a href="mailto:"><i className="fa fa-envelope fa-lg"></i></a></li>
+                        </ul>
+                    </figure>
+                    <h4>Tony Stark</h4>
+                    <p>Web Developer</p>
+                </div>
+            </div>
+
+            <div className="col-md-3">
+                <div className="team-member">
+                    <figure>
+                        <img src="https://i.pinimg.com/originals/f5/a7/f2/f5a7f286dca7652e15cf413e5efe2702.png" />
+                        <ul>
+                          <li><a href="https://www.facebook.com/" target="_blank"><i className="fa fa-facebook fa-lg"></i></a></li>
+                          <li><a href="https://www.linkedin.com/" target="_blank"><i className="fa fa-linkedin fa-lg"></i></a></li>
+                          <li><a href="mailto:"><i className="fa fa-envelope fa-lg"></i></a></li>
+                        </ul>
+                    </figure>
+                    <h4>Tony Stark</h4>
+                    <p>Web Developer</p>
+                </div>
+            </div>
+
+            <div className="col-md-3">
+                <div className="team-member">
+                    <figure>
+                        <img src="https://i.pinimg.com/originals/f5/a7/f2/f5a7f286dca7652e15cf413e5efe2702.png" />
+                        <ul>
+                          <li><a href="https://www.facebook.com/" target="_blank"><i className="fa fa-facebook fa-lg"></i></a></li>
+                          <li><a href="https://www.linkedin.com/" target="_blank"><i className="fa fa-linkedin fa-lg"></i></a></li>
+                          <li><a href="mailto:"><i className="fa fa-envelope fa-lg"></i></a></li>
+                        </ul>
+                    </figure>
+                    <h4>Tony Stark</h4>
+                    <p>Web Developer</p>
+                </div>
+            </div>
+
+            <div className="col-md-3">
+                <div className="team-member">
+                    <figure>
+                        <img src="https://i.pinimg.com/originals/f5/a7/f2/f5a7f286dca7652e15cf413e5efe2702.png" />
+                        <ul>
+                          <li><a href="https://www.facebook.com/" target="_blank"><i className="fa fa-facebook fa-lg"></i></a></li>
+                          <li><a href="https://www.linkedin.com/" target="_blank"><i className="fa fa-linkedin fa-lg"></i></a></li>
+                          <li><a href="mailto:"><i className="fa fa-envelope fa-lg"></i></a></li>
+                        </ul>
+                    </figure>
+                    <h4>Tony Stark</h4>
+                    <p>Web Developer</p>
+                </div>
+            </div>
+
+            <div className="col-md-3">
+                <div className="team-member">
+                    <figure>
+                        <img src="https://i.pinimg.com/originals/f5/a7/f2/f5a7f286dca7652e15cf413e5efe2702.png" />
+                        <ul>
+                          <li><a href="https://www.facebook.com/" target="_blank"><i className="fa fa-facebook fa-lg"></i></a></li>
+                          <li><a href="https://www.linkedin.com/" target="_blank"><i className="fa fa-linkedin fa-lg"></i></a></li>
+                          <li><a href="mailto:"><i className="fa fa-envelope fa-lg"></i></a></li>
+                        </ul>
+                    </figure>
+                    <h4>Tony Stark</h4>
+                    <p>Web Developer</p>
+                </div>
+            </div>
+
+            <div className="col-md-3">
+                <div className="team-member">
+                    <figure>
+                        <img src="https://i.pinimg.com/originals/f5/a7/f2/f5a7f286dca7652e15cf413e5efe2702.png" />
+                        <ul>
+                          <li><a href="https://www.facebook.com/" target="_blank"><i className="fa fa-facebook fa-lg"></i></a></li>
+                          <li><a href="https://www.linkedin.com/" target="_blank"><i className="fa fa-linkedin fa-lg"></i></a></li>
+                          <li><a href="mailto:"><i className="fa fa-envelope fa-lg"></i></a></li>
+                        </ul>
+                    </figure>
+                    <h4>Tony Stark</h4>
+                    <p>Web Developer</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+</div>
   )
 }
