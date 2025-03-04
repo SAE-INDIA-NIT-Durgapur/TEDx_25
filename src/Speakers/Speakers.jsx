8a4d232960df88afab8035a1a2d9d5b8f89cb872
Sachin Kumar Yadav <yadavsachin.kr014@gmail.com>
2025-03-04 15:53:12 +0530

    Speakers page updated

--- src/Speakers/Speakers.jsx
+++ src/Speakers/Speakers.jsx
@@ -1,90 +1,72 @@
-import React from 'react';
-import { motion } from 'framer-motion';
-import './Speakers.css'
-import Navbar from '../Navbar/Navbar';
-import Footer from '../Footer/Footer';
+import React from "react";
+import { motion } from "framer-motion";
+import "./Speakers.css";
+import Navbar from "../Navbar/Navbar";
+import Footer from "../Footer/Footer";
+
+const speakersData = [
+  {
+    name: "RUPESH MAHORE",
+    image:
+      "https://635124c34eeff312c9586ae2--subtle-rabanadas-ef52a0.netlify.app/static/media/rupesh.b713abf3.jpeg",
+    details: '"Cybersecurity Expert | Speaker at DEFCON 2023"',
+  },
+  {
+    name: "Lara",
+    image:
+      "https://635124c34eeff312c9586ae2--subtle-rabanadas-ef52a0.netlify.app/static/media/rupesh.b713abf3.jpeg",
+    details: "AI Researcher | Guest Lecturer at MIT",
+  },
+  {
+    name: "Jack",
+    image:
+      "https://635124c34eeff312c9586ae2--subtle-rabanadas-ef52a0.netlify.app/static/media/rupesh.b713abf3.jpeg",
+    details: "Ethical Hacker | Author of 'Hacking Unleashed'",
+  },
+];
+
 const Speakers = () => {
   return (
     <div>
-    <Navbar/>
-       <motion.div className='sp-head-container'>
-    <motion.h1  initial={{opacity:0}} animate={{opacity:1}} transition={{duration:2, staggerChildren:2}} className='speakers_headings'>Our Speakers</motion.h1>
-    </motion.div>
-<motion.div  initial={{y:110, opacity:0}} animate={{y:0, opacity:1}} transition={{duration:1}} className='Speakers-body'>
-  <div className="Speakers-cards-container">
+      <Navbar />
+      <motion.div className="sp-head-container">
+        <motion.h1
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 2 }}
+          className="speakers_headings"
+        >
+          Our Speakers
+        </motion.h1>
+      </motion.div>
 
-    <div className="Speakers_contain" id="rupesh">
-      <div className="Speakers_inner">
-        <div className="Sp-text">
-          <h1>
-            RUPESH MAHORE
-          </h1>
-          <p>He is a developer and Programmer. he is also a great person. He founded XYZ comapany </p>
-                  </div>
-      </div>
-    </div>
-
-    <div className="Speakers_contain" id="kishwar">
-      <div className="Speakers_inner">
-        <div className="Sp-text">
-          <h1>
-            Lara
-          </h1>
+      <motion.div
+        initial={{ y: 110, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+        transition={{ duration: 1 }}
+        className="Speakers-body"
+      >
+        <div className="Speakers-cards-container">
+          {speakersData.map((speaker, index) => (
+            <div key={index} className="Speakers_contain">
+              <div className="Speakers_inner">
+                <img
+                  src={speaker.image}
+                  alt={speaker.name}
+                  className="Speaker-image"
+                />
+                <div className="Speaker-overlay">
+                  <h3>{speaker.name}</h3>
+                  <p>{speaker.details}</p>
+                </div>
+              </div>
+            </div>
+          ))}
         </div>
-      </div>
+      </motion.div>
+      <Footer />
     </div>
-    <div className="Speakers_contain" id="munish">
-      <div className="Speakers_inner">
-        <div className="Sp-text">
-          <h1>
-            Jack
-          </h1>
-          <span> Curator </span>
-      
-        </div>
-      </div>
-    </div>
+  );
+};
 
-    <div className="Speakers_contain" id="kamna">
-      <div className="Speakers_inner">
-        <div className="Sp-text">
-          <h1>
-            Jack
-          </h1>
-          <span> Curator </span>
-      
-        </div>
-      </div>
-    </div>
-
-    <div className="Speakers_contain" id="munish">
-      <div className="Speakers_inner">
-        <div className="Sp-text">
-          <h1>
-            Jack
-          </h1>
-          <span> Curator </span>
-      
-        </div>
-      </div>
-    </div>
-
-    <div className="Speakers_contain" id="kamna">
-      <div className="Speakers_inner">
-        <div className="Sp-text">
-          <h1>
-            Jack
-          </h1>
-          <span> Curator </span>
-      
-        </div>
-      </div>
-    </div>
-  </div>
-    </motion.div>
-    <Footer/>
-    </div>
-  )
-}
-
-export default Speakers
+export default Speakers;
