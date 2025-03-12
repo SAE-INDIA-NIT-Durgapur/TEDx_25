4ef2af3511e7c68ff416d7e36f2576cc426864b1
Monisha <163467495+Monisha2417@users.noreply.github.com>
2025-03-12 19:57:03 +0530

    added past speakers

--- src/Speakers/Speakers.jsx
+++ src/Speakers/Speakers.jsx
@@ -25,6 +25,28 @@ const speakersData = [
   },
 ];
 
+const pastSpeakersData = [
+  {
+    name: "Alice",
+    image:
+      "https://635124c34eeff312c9586ae2--subtle-rabanadas-ef52a0.netlify.app/static/media/rupesh.b713abf3.jpeg",
+    details: "Blockchain Specialist | TEDx Speaker",
+  },
+  {
+    name: "Bob",
+    image:
+      "https://635124c34eeff312c9586ae2--subtle-rabanadas-ef52a0.netlify.app/static/media/rupesh.b713abf3.jpeg",
+    details: "Quantum Computing Expert | Author",
+  },
+  {
+    name: "Charlie",
+    image:
+      "https://635124c34eeff312c9586ae2--subtle-rabanadas-ef52a0.netlify.app/static/media/rupesh.b713abf3.jpeg",
+    details: "Data Scientist | Forbes 30 Under 30",
+  },
+  
+];
+
 const Speakers = () => {
   return (
     <div>
@@ -64,9 +86,47 @@ const Speakers = () => {
           ))}
         </div>
       </motion.div>
+
+      <motion.div className="sp-head-container">
+        <motion.h1
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 2 }}
+          className="speakers_headings"
+        >
+          Past Speakers
+        </motion.h1>
+      </motion.div>
+
+      <motion.div
+        initial={{ y: 110, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+        transition={{ duration: 1 }}
+        className="Speakers-body"
+      >
+        <div className="Speakers-cards-container">
+          {pastSpeakersData.map((speaker, index) => (
+            <div key={index} className="Speakers_contain">
+              <div className="Speakers_inner">
+                <img
+                  src={speaker.image}
+                  alt={speaker.name}
+                  className="Speaker-image"
+                />
+                <div className="Speaker-overlay">
+                  <h3>{speaker.name}</h3>
+                  <p>{speaker.details}</p>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </motion.div>
+
       <Footer />
     </div>
   );
 };
 
 export default Speakers;
+
